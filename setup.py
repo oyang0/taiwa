efb70ab1c6d037e39137515a5221d9287d42f311
Oliver Yang <oyang@ualberta.ca>
2024-08-26 09:49:55 -0600

    Set up only necessary schemas

--- setup.py
+++ setup.py
@@ -4,34 +4,33 @@ import psycopg2
 conn = psycopg2.connect(os.environ["DATABASE_URL"])
 cur = conn.cursor()
 
-for stage in ("taiwa_staging", "taiwa_production"):
-	cur.execute(f"CREATE SCHEMA IF NOT EXISTS {stage};")
+cur.execute(f"CREATE SCHEMA IF NOT EXISTS {os.environ["SCHEMA"]};")
 
-	cur.execute(f"""
-	CREATE TABLE IF NOT EXISTS {stage}.leitner (
-		id SERIAL PRIMARY KEY,
-		sender TEXT NOT NULL UNIQUE,
-		system TEXT NOT NULL
-	);
-	""")
+cur.execute(f"""
+CREATE TABLE IF NOT EXISTS {os.environ["SCHEMA"]}.leitner (
+	id SERIAL PRIMARY KEY,
+	sender TEXT NOT NULL UNIQUE,
+	system TEXT NOT NULL
+);
+""")
 
-	cur.execute(f"""
-	CREATE TABLE IF NOT EXISTS {stage}.answers (
-		id SERIAL PRIMARY KEY,
-		sender TEXT NOT NULL UNIQUE,
-		answer TEXT NOT NULL,
-		options TEXT NOT NULL,
-		expression_id INTEGER NOT NULL
-	);
-	""")
+cur.execute(f"""
+CREATE TABLE IF NOT EXISTS {os.environ["SCHEMA"]}.answers (
+	id SERIAL PRIMARY KEY,
+	sender TEXT NOT NULL UNIQUE,
+	answer TEXT NOT NULL,
+	options TEXT NOT NULL,
+	expression_id INTEGER NOT NULL
+);
+""")
 
-	cur.execute(f"""
-	CREATE TABLE IF NOT EXISTS {stage}.problems (
-		id SERIAL PRIMARY KEY,
-		sender TEXT NOT NULL,
-		problem TEXT NOT NULL
-	);
-	""")
+cur.execute(f"""
+CREATE TABLE IF NOT EXISTS {os.environ["SCHEMA"]}.problems (
+	id SERIAL PRIMARY KEY,
+	sender TEXT NOT NULL,
+	problem TEXT NOT NULL
+);
+""")
 
 conn.commit()
 cur.close()
